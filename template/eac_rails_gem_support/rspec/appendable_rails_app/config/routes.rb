30d7c8667da6f1bafd92a331e794d78404fb464a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-08-17 23:25:34 -0300

    EacRailsGemSupport::Rspec::AppendableRailsApp: fix application template.

--- template/eac_rails_gem_support/rspec/appendable_rails_app/config/routes.rb
+++ template/eac_rails_gem_support/rspec/appendable_rails_app/config/routes.rb
@@ -1,10 +1,4 @@
 # frozen_string_literal: true
 
-Rails.application.routes.draw do
-  resources :jobs do
-    collection do
-      get 'search'
-    end
-  end
-  resources :users
+::Rails.application.routes.draw do
 end
