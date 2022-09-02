6c81bd2485d8266cbd4a0f01b41a8285c8138eb9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-09-01 21:13:49 -0300

    EacRailsGemSupport::Rspec::Setup.setup_minitest_support.

--- lib/eac_rails_gem_support/rspec/setup.rb
+++ lib/eac_rails_gem_support/rspec/setup.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'eac_rails_gem_support/rspec/minitest_support'
 require 'eac_ruby_utils/core_ext'
 
 module EacRailsGemSupport
@@ -9,6 +10,7 @@ module EacRailsGemSupport
       require_sub __FILE__, include_modules: true
 
       def self.extended(obj)
+        obj.setup_minitest_support
         obj.singleton_class.include(::EacRubyUtils::SimpleCache)
         obj.setup_rails_env
         return unless obj.setup_rails_app
@@ -16,6 +18,10 @@ module EacRailsGemSupport
         %w[fixtures capybara factory_bot].each do |item|
           obj.send("setup_#{item}")
         end
+      end
+
+      def setup_minitest_support
+        rspec_config.include ::EacRailsGemSupport::Rspec::MinitestSupport
       end
     end
   end
