3453a32d5f64e2f059b2a9226310649b635ac6ad
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-09-01 21:10:55 -0300

    EacRailsGemSupport::Rspec::MinitestSupport.

---
+++ lib/eac_rails_gem_support/rspec/minitest_support.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module EacRailsGemSupport
+  module Rspec
+    module MinitestSupport
+      def assert_difference(code, message = nil, compare_method = :not_to, &block)
+        before_value = assert_difference_call(code)
+        block.call
+        after_value = assert_difference_call(code)
+        expect(after_value).send(compare_method, eq(before_value), message)
+      end
+
+      def assert_no_difference(code, message = nil, &block)
+        assert_difference(code, message, :to, &block)
+      end
+
+      def assert_equal(expected_value, actual_value, message = nil)
+        expect(actual_value).to eq(expected_value), message
+      end
+
+      def assert_not(expected_value, message = nil)
+        expect(expected_value).to be_falsy, message
+      end
+
+      def assert_not_equal(expected_value, actual_value, message = nil)
+        expect(actual_value).not_to eq(expected_value), message
+      end
+
+      private
+
+      def assert_difference_call(code)
+        if code.is_a?(::Proc)
+          code.call
+        elsif code.is_a?(::String)
+          eval(code) # rubocop:disable Security/Eval
+        else
+          raise "Unmapped type for \"#{code}\""
+        end
+      end
+    end
+  end
+end
