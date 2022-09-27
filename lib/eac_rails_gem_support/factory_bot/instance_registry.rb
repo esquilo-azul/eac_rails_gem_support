f8cb673cee787c748df6b1a0043c741f85581b74
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-09-27 11:21:26 -0300

    EacRailsGemSupport::FactoryBot::InstanceRegistry.

---
+++ lib/eac_rails_gem_support/factory_bot/instance_registry.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module EacRailsGemSupport
+  module FactoryBot
+    class InstanceRegistry
+      class << self
+        def default
+          @default ||= ::EacRailsGemSupport::FactoryBot::InstanceRegistry.new
+        end
+
+        delegate :fetch, :reset, to: :default
+
+        def sanitize_key(key)
+          key.to_sym
+        end
+      end
+
+      def fetch(key)
+        key = self.class.sanitize_key(key)
+        if fetched.key?(key)
+          fetched.fetch(key)
+        else
+          fetched[key] = ::FactoryBot.create(key)
+        end
+      end
+
+      def reset
+        @fetched = nil
+      end
+
+      private
+
+      def fetched
+        @fetched ||= {}
+      end
+    end
+  end
+end
