6a996c2b9a9873cbc2bd5c43741099f39d15d9c1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-09-27 10:33:12 -0300

    RSpec::Core::Configuration.factory_bot_fetch.

--- lib/eac_rails_gem_support/rspec/setup/factory_bot.rb
+++ lib/eac_rails_gem_support/rspec/setup/factory_bot.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'eac_ruby_utils/core_ext'
+require 'eac_rails_gem_support/factory_bot/instance_registry'
 
 module EacRailsGemSupport
   module Rspec
@@ -10,6 +11,14 @@ module EacRailsGemSupport
           require 'factory_bot_rails'
           rspec_config.include ::FactoryBot::Syntax::Methods
           Spring.after_fork { ::FactoryBot.reload } if defined?(Spring)
+          rspec_config.include HelpMethods
+          rspec_config.after { ::EacRailsGemSupport::FactoryBot::InstanceRegistry.reset }
+        end
+
+        module HelpMethods
+          def factory_bot_fetch(key)
+            ::EacRailsGemSupport::FactoryBot::InstanceRegistry.fetch(key)
+          end
         end
       end
     end
