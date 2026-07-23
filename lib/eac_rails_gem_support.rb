eafa117bd7ca517e0ba1fd319cd780444da155e0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-22 23:37:26 -0300

    EacRailsGemSupport: refact with EacRubyBase1::RootModuleSetup.

--- lib/eac_rails_gem_support.rb
+++ lib/eac_rails_gem_support.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils'
-EacRubyUtils::RootModuleSetup.perform __FILE__ do
+require 'eac_ruby_base1'
+EacRubyBase1::RootModuleSetup.perform __FILE__ do
   ignore 'rspec/shared_examples/**/*'
+  require 'avm/eac_ruby_base1'
+  require 'capybara'
+  require 'eac_ruby_gem_support'
+  require 'eac_templates'
+  require 'factory_bot_rails'
 end
-
-module EacRailsGemSupport
-end
-
-require 'avm/eac_ruby_base1'
-require 'capybara'
-require 'eac_ruby_gem_support'
-require 'eac_templates'
-require 'factory_bot_rails'
